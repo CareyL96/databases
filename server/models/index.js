e66227ed41a476f217d10f1527505f72e012473b
careyl96 <careyl96@gmail.com>
2018-03-30 21:08:00 -0700

    added SQL code to model

--- server/models/index.js
+++ server/models/index.js
@@ -2,13 +2,42 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function () {
+      db.connect(function (err) {
+        if (err) throw err;
+        db.query(`SELECT Text 
+                  FROM messages 
+                  WHERE RoomID 
+                  IN 
+                  (SELECT RoomID 
+                  FROM rooms 
+                  WHERE Roomname = classname?????)`)
+      });
+    }, // a function which produces all the messages
+    post: function () {
+
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
     // Ditto as above.
-    get: function () {},
+    get: function () {
+      db.connect(function (err) {
+        if (err) throw err;
+        db.query(`SELECT Text 
+                  FROM messages 
+                  WHERE RoomID
+                  IN 
+                  (SELECT RoomID 
+                  FROM rooms 
+                  WHERE Roomname = classname?????)
+                  AND UserID 
+                  IN
+                  (SELECT UserID
+                  FROM users
+                  WHERE Username = users????????`);
+      });
+    },
     post: function () {}
   }
 };
--- server/schema.sql
+++ server/schema.sql
@@ -1,11 +1,29 @@
+drop database if exists chat;
 CREATE DATABASE chat;
 
 USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  MessageID INTEGER(1000) NOT NULL, 
+  UserID INTEGER(15),
+  Text CHARACTER(100), 
+  RoomID INTEGER(15),
+  CreatedAt TIMESTAMP,
+  PRIMARY KEY (MessageID),
+  FOREIGN KEY (UserID) REFERENCES users(UserID),
+  FOREIGN KEY (RoomID) REFERENCES rooms(RoomID)
 );
 
+CREATE TABLE users (
+  UserID INTEGER(1000) NOT NULL,
+  Username CHARACTER(15)
+);
+
+CREATE TABLE rooms (
+  RoomID INTEGER(100) NOT NULL,
+  Roomname CHARACTER(15)
+);
 /* Create other tables and define schemas for them here! */
 
 
